f1041400e7fcef0179b29aeebde7e5497955432a
Pubudusandeepa <ppsandeepa12@gmail.com>
2021-06-07 12:50:55 +0530

    create static paths

--- backend/settings.py
+++ backend/settings.py
@@ -122,10 +122,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL='/images/'
+
+STATICFILES_DIRS=[
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT ='static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
 
 CORS_ALLOW_ALL_ORIGINS = True
