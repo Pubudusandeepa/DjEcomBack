6e95361fc12f2415428a20f7a17baeecafbc7b7f
Pubudusandeepa <ppsandeepa12@gmail.com>
2021-06-10 13:08:00 +0530

    add permission classes to view

--- base/views.py
+++ base/views.py
@@ -1,8 +1,9 @@
-from django.contrib.auth.models import update_last_login
+from django.contrib.auth.models import User, update_last_login
 from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework import serializers
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser 
 from rest_framework.response import Response
 
 
@@ -43,17 +44,26 @@ def getRoutes(request):
     return Response(routes)
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user =request.user
     serializer = UserSerializer(user,many=False)
     return Response(serializer.data)
 
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users,many=True)
+    return Response(serializer.data)  
 
 @api_view(['GET'])
 def getProducts(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products,many=True)
     return Response(serializer.data)
+
+  
 
 @api_view(['GET'])
 def getProduct(request, pk):
