477307c52b23c7074c923f45138d9a8aeb023410
Pubudusandeepa <ppsandeepa12@gmail.com>
2021-06-10 22:17:52 +0530

    create product views

---
+++ base/views/product_views.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import User, update_last_login
+from django.shortcuts import render
+from rest_framework import serializers
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser 
+from rest_framework.response import Response
+
+
+from base.products import products
+from base.models import Product
+from base.serializer import ProductSerializer
+# Create your views here.
+
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
+
+from base import serializer
+
+
+
+@api_view(['GET'])
+def getProducts(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products,many=True)
+    return Response(serializer.data)
+
+  
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = Product.objects.get(_id = pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)   
+
