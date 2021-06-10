4da33018af50ba049557984abd7bc340ae449682
Pubudusandeepa <ppsandeepa12@gmail.com>
2021-06-10 12:43:04 +0530

    modify  MyTokenObtainPairSerializer

--- base/views.py
+++ base/views.py
@@ -8,7 +8,7 @@ from rest_framework.response import Response
 
 from .products import products
 from .models import Product
-from .serializer import ProductSerializer,UserSerializer
+from .serializer import ProductSerializer,UserSerializer,UserSerializerWithToken
 # Create your views here.
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -16,11 +16,10 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
-        refresh = self.get_token(self.user)
-
-        data['username'] = self.user.username
-        data['email'] = self.user.email
-
+       
+        serializer = UserSerializerWithToken(self.user).data
+        for k,v in serializer.items():
+            data[k] = v
      
 
         return data    
